19ed468c8718926cbe89b411e0d58d53ddf31312
Niki Amini-Naieni <niki@adainnovations.ai>
2024-01-14 00:36:06 +0000

    added weight back

--- mmdet/models/losses/my_loss.py
+++ mmdet/models/losses/my_loss.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from mmdet.registry import MODELS
 from .utils import weighted_loss
 
+@weighted_loss
 def my_loss(pred, target):
     assert pred.size() == target.size() and target.numel() > 0
     box_centers_x_pred = pred[:, 0] + pred[:, 2] / 2
@@ -30,7 +31,13 @@ class MyLoss(nn.Module):
                 weight=None,
                 avg_factor=None,
                 reduction_override=None):
-        print(weight)
+        assert reduction_override in (None, 'none', 'mean', 'sum')
+        assert weight[:, 0] == weight[:, 1] and weight[:, 1] == weight[:, 2] and weight[:, 2] == weight[:, 3]
+
+        weight = weight[:, :2]
+        reduction = (
+            reduction_override if reduction_override else self.reduction)
+        
         loss_bbox = self.loss_weight * my_loss(
-            pred, target).mean()
+            pred, target, weight, reduction=reduction, avg_factor=avg_factor)
         return loss_bbox
